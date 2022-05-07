3b46e7dc7191a8b2b008ee16be5f5eb60997dc41
ungchun <leedool3003@naver.com>
2022-05-07 18:56:38 +0900

    feat: add lottie loading file

--- hongkimDaonProject/hongkimDaonProject/Controller/StartPageViewController.swift
+++ hongkimDaonProject/hongkimDaonProject/Controller/StartPageViewController.swift
@@ -1,35 +1,73 @@
 import UIKit
 import FirebaseAuth
 import FirebaseFirestore
+import Lottie
 
 class StartPageViewController: UIViewController {
     let database = Firestore.firestore()
+    var animationView: AnimationView = {
+        let lottieView = AnimationView(name: "lottieFile")
+        lottieView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        lottieView.contentMode = .scaleAspectFill
+        return lottieView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("start viewLoad")
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        if let user = Auth.auth().currentUser {
-            print("view user \(user.uid)")
-            let docRef = self.database.document("user/\(user.uid)")
-            docRef.getDocument { snapshot, error in
-                if let error = error {
-                    print("DEBUG: \(error.localizedDescription)")
-                    return
+        animationView.center = CGPoint(x: view.frame.size.width  / 2, y: view.frame.size.height / 2.3)
+        //        animationView.center = view.center
+        animationView.loopMode = .playOnce
+        view.addSubview(animationView)
+        animationView.play {(finish) in
+            self.animationView.play()
+            if let user = Auth.auth().currentUser {
+                print("view user \(user.uid)")
+                let docRef = self.database.document("user/\(user.uid)")
+                docRef.getDocument { snapshot, error in
+                    if let error = error {
+                        print("DEBUG: \(error.localizedDescription)")
+                        return
+                    }
+                    guard let exist = snapshot?.exists else {return}
+                    if exist == true {
+                        self.animationView.removeFromSuperview()
+                        self.showMainViewController()
+                    } else {
+                        self.animationView.removeFromSuperview()
+                        self.showLoginViewController()
+                    }
                 }
-                guard let exist = snapshot?.exists else {return}
-                if exist == true {
-                    sleep(1)
-                    self.showMainViewController()
-                } else {
-                    sleep(1)
-                    self.showLoginViewController()
-                }
+            } else {
+                self.animationView.removeFromSuperview()
+                self.showLoginViewController()
             }
-        } else {
-            sleep(1)
-            self.showLoginViewController()
         }
+    }
+    // 일단 요거 지우지말고 대기
+    override func viewDidAppear(_ animated: Bool) {
+        //        if let user = Auth.auth().currentUser {
+        //            print("view user \(user.uid)")
+        //            let docRef = self.database.document("user/\(user.uid)")
+        //            docRef.getDocument { snapshot, error in
+        //                if let error = error {
+        //                    print("DEBUG: \(error.localizedDescription)")
+        //                    return
+        //                }
+        //                guard let exist = snapshot?.exists else {return}
+        //                if exist == true {
+        //                    sleep(1)
+        //                    self.animationView.stop()
+        //                    self.showMainViewController()
+        //                } else {
+        //                    sleep(1)
+        //                    self.animationView.stop()
+        //                    self.showLoginViewController()
+        //                }
+        //            }
+        //        } else {
+        //            sleep(1)
+        //            self.animationView.stop()
+        //            self.showLoginViewController()
+        //        }
     }
 }
 
