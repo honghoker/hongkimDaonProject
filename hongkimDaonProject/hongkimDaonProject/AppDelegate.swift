ef820a625c3de3e73cc859b491812c372d0ab583
ungchun <leedool3003@naver.com>
2022-05-10 11:48:45 +0900

    feat: app delegate

--- hongkimDaonProject/hongkimDaonProject/AppDelegate.swift
+++ hongkimDaonProject/hongkimDaonProject/AppDelegate.swift
@@ -71,12 +71,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?,
                      annotation: Any) -> Bool {
-        if let dynamicLink = DynamicLinks.dynamicLinks().dynamicLink(fromCustomSchemeURL: url) {
-            // Handle the deep link. For example, show the deep-linked content or
-            // apply a promotional offer to the user's account.
-            // ...
-            return true
-        }
         return false
     }
     // MARK: UISceneSession Lifecycle
