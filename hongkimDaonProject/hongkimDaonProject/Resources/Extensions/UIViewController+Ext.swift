63643e4fce1d95c9978d3757854e696c9c6a60e2
honghoker <ekflzhftk@naver.com>
2022-10-27 16:27:48 +0900

    UIViewController HideKeyboard onTap

---
+++ hongkimDaonProject/hongkimDaonProject/Resources/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// MARK: outSide touch 하면 inputView dismiss
+extension UIViewController {
+    func setupHideKeyboardOnTap() {
+        self.view.addGestureRecognizer(self.endEditingRecognizer())
+        self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
+    }
+
+    private func endEditingRecognizer() -> UIGestureRecognizer {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        return tap
+    }
+}
