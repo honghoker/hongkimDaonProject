596909bfe1997b7cf39e0eb8809804a560695a2b
honghoker <ekflzhftk@naver.com>
2023-09-02 16:47:00 +0900

    [remove] CocoaPods

--- hongkimDaonProject/Pods/Target Support Files/FMPhotoPicker/FMPhotoPicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FMPhotoPicker : NSObject
-@end
-@implementation PodsDummy_FMPhotoPicker
-@end
--- hongkimDaonProject/Pods/Target Support Files/STTextView/STTextView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_STTextView : NSObject
-@end
-@implementation PodsDummy_STTextView
-@end
